bc662d620e0f6defd9b54da4b453082dbf0445c2
Thomas Taege <thomas.taege@gmail.com>
2014-10-22 00:03:58 +1000

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Thomas Taege on 22/10/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
