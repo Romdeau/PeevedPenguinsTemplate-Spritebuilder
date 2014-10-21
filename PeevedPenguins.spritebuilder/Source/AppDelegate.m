b62b2267d4d330d8d094c702f86e40089d3d2cd4
Thomas Taege <thomas.taege@gmail.com>
2014-10-21 22:40:38 +1000

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/AppDelegate.m
+++ PeevedPenguins.spritebuilder/Source/AppDelegate.m
@@ -54,11 +54,6 @@
     
     [self setupCocos2dWithOptions:cocos2dSetup];
     
-    [self setupCocos2dWithOptions:cocos2dSetup];
-    
-    [CCBReader load:@"Penguin"];
-    [CCBReader load:@"Seal"];
-    
     return YES;
 }
 
